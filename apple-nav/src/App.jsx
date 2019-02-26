5b556f78fd9c6b9fc76f6d088f4788feb4db4422
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-02-26 15:43:14 +0000

    Blank Project Starting Point

---
+++ apple-nav/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div>A New Beginning</div>
+      </>
+    );
+  }
+}
+
+export default App;
